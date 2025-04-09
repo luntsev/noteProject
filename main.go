f79eb8589163ed9f6820a4a0f8c8ba20d93c9e8e
Nikolay Luntsev <n.luntsev@yandex.ru>
2025-04-09 20:36:16 +0300

    Добавил пакет server

--- main.go
+++ main.go
@@ -1,16 +1,11 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import "noteProject/server"
+
+func init() {
+	server.InitServer()
+}
 
 func main() {
-	// Создайте новый роутер по умолчанию у gin
-	router := gin.Default()
-	// Регистрируем обработчик GET-запроса /ping
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Привет я GIN",
-		})
-	})
-	// Запускаем роутер
-	router.Run(":9100")
+	server.StartServer()
 }
