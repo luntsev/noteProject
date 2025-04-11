e034e10d0f205b154c51fff020f8d4d6b65edc4b
Nikolay Luntsev <n.luntsev@yandex.ru>
2025-04-11 17:52:28 +0300

    Начал добавлять апдейт

--- handlers/note_handlers.go
+++ handlers/note_handlers.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 // Обработка запроса для создания заметки
@@ -38,8 +39,6 @@ func CreateNoteHandler(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{
 		"note":    note,
 		"message": "Заметка успешно создана"})
-
-	ctx.JSON(http.StatusOK, "CreateNoteHandler")
 }
 
 // Обработка запроса для получения заметки по ID
@@ -54,10 +53,22 @@ func GetNotesHandler(ctx *gin.Context) {
 
 // Обработка запроса для удаления заметки по ID
 func DeleteNoteHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, "DeleteNoteHandler")
+	id := ctx.Param("id")
+	collection := database.MongoClient.Database("admin").Collection(fmt.Sprintf("notes/%d", 1))
+	filter := bson.M{"id": id}
+
+	if result, err := collection.DeleteOne(ctx, filter); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	} else if result.DeletedCount == 0 {
+		ctx.JSON(http.StatusOK, "Заметка не найдена")
+	} else {
+		ctx.JSON(http.StatusOK, "Заметка успешно удалена")
+	}
 }
 
 // Обработка запроса для редактирования заметки по ID
 func UpdateNoteHandler(ctx *gin.Context) {
+	authorId := 1
+
 	ctx.JSON(http.StatusOK, "UpdateNoteHandler")
 }
